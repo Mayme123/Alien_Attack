fb97049ea1eb3c4ce08a1f898ac49e9b01a29f88
Matt <mayme123@live.com>
2024-08-19 00:46:10 -0400

    Implemented kill scoring
    completion of video 47

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,13 +1,14 @@
 extends Area2D
 
 @export var enemy_speed = 180
+signal died
 
 func _physics_process(delta):
 	global_position.x -= enemy_speed * delta
 	
 func die():
 	queue_free()
-
+	emit_signal("died")
 
 func _on_body_entered(body):
 	body.take_damage()
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var lives = 3
+var score = 0
 @onready var player = $Player
 
 func _on_deathzone_area_entered(area):
@@ -14,3 +15,11 @@ func _on_player_took_damage():
 		player.die()
 	
 	print(lives)
+
+func _on_enemy_spawner_enemy_spawned(enemy_instance):
+	enemy_instance.connect("died", on_enemy_died)
+	add_child(enemy_instance)
+	
+func on_enemy_died():
+	score += 1
+	print(score)
