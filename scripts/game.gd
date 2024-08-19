eac9f41c7066e67947dcc57e7391205a8e0fe993
Matt <mayme123@live.com>
2024-08-19 01:02:59 -0400

    Score UI
    completion of video 48

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,23 +3,24 @@ extends Node
 var lives = 3
 var score = 0
 @onready var player = $Player
+@onready var hud = $UI/HUD
+
+func _ready():
+	hud.set_score_label(score)
 
 func _on_deathzone_area_entered(area):
 	area.die()
-
 
 func _on_player_took_damage():
 	lives -= 1
 	if(lives == 0):
 		print("Game Over")
 		player.die()
-	
-	print(lives)
 
 func _on_enemy_spawner_enemy_spawned(enemy_instance):
 	enemy_instance.connect("died", on_enemy_died)
 	add_child(enemy_instance)
 	
 func on_enemy_died():
-	score += 1
-	print(score)
+	score += 100
+	hud.set_score_label(score)
